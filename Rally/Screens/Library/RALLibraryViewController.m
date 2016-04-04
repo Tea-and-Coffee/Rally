30e7ffcb9bb9148f790a2f35dab275753f7a3335
Arai <arai@yazashin.local>
2016-04-04 18:06:07 +0900

    蔵書検索のJSON取得

--- Rally/Screens/Library/RALLibraryViewController.m
+++ Rally/Screens/Library/RALLibraryViewController.m
@@ -19,6 +19,7 @@
     
     [self fetchLibrary];
     [self fetchCheck];
+    [self fetchCheckWithSession:@"43eb4d5ce6f0372681f7dfc3971f68c4"];
 }
 
 - (void)fetchLibrary {
@@ -56,6 +57,27 @@
     [parameters setObject:@"4834000826" forKey:@"isbn"];
     [parameters setObject:@"Tokyo_Adachi" forKey:@"systemid"];
     [parameters setObject:@"Univ_Tokyomirai" forKey:@"systemid"];
+    [parameters setObject:@"json" forKey:@"format"];
+    
+    [manager GET:string parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSData *trimmedData = [self trimmedDataWithResponseObject:responseObject];
+        
+        NSError *error;
+        NSDictionary *_check = [NSJSONSerialization JSONObjectWithData:trimmedData options:0 error:&error];
+        RALCheck *check = [[RALCheck alloc] initWithDictionary:_check];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+    }];
+}
+
+- (void)fetchCheckWithSession:(NSString *)session {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    NSString *string = @"https://api.calil.jp/check";
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    [parameters setObject:@"863e4a1924a6c4add0119d07efaf5835" forKey:@"appkey"];
+    [parameters setObject:session forKey:@"session"];
     [parameters setObject:@"json" forKey:@"format"];
     
     [manager GET:string parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
