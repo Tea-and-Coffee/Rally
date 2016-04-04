cfcab14b0fe6b28048c4e8f0c418af36e0612841
Arai <arai@yazashin.local>
2016-04-04 12:30:57 +0900

    図書館のJSON取得

--- Rally/RALLibraryViewController.m
+++ Rally/RALLibraryViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "RALLibraryViewController.h"
+#import "RALLibrary.h"
+#import "AFNetworking.h"
 
 @interface RALLibraryViewController ()
 
@@ -17,17 +19,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.URL = [NSURL URLWithString:@"https://api.calil.jp/library?"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:self.URL];
-    [request setValue:@"863e4a1924a6c4add0119d07efaf5835" forHTTPHeaderField:@"appkey"];
-    [request setValue:@"東京都" forHTTPHeaderField:@"pref"];
-    [request setValue:@"足立区" forHTTPHeaderField:@"city"];
-    [request setValue:@"json" forKey:@"format"];
-    request.HTTPMethod = @"GET";
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *datatask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"a");
+    NSString *string = @"https://api.calil.jp/library";
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    [parameters setObject:@"863e4a1924a6c4add0119d07efaf5835" forKey:@"appkey"];
+    [parameters setObject:@"東京都" forKey:@"pref"];
+    [parameters setObject:@"足立区" forKey:@"city"];
+    [parameters setObject:@"json" forKey:@"format"];
+    [parameters setObject:@"" forKey:@"callback"];
+    
+    [manager GET:string parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSError *error;
+        NSArray *_libraries = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:&error];
+        NSMutableArray *libraries = [NSMutableArray array];
+        for (NSDictionary *_library in _libraries) {
+            RALLibrary *library = [[RALLibrary alloc] initWithDictionary:_library];
+            [libraries addObject:library];
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
     }];
 }
 
